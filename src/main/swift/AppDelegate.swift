f8ecc6328574ecb2ac0bffe7116d3996eb774898
Venkat Peri <venkatperi@gmail.com>
2015-05-17 21:31:21 -0400

    initial

---
+++ src/main/swift/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  template
+//
+//  Created by Venkat Peri on 5/5/15.
+//  Copyright (c) 2015 Template Inc. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  @IBOutlet weak var window: NSWindow!
+
+
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
